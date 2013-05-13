709d7b9c7f4546ebd2af39bd522dad020faf8d7d
Jonathan Olson <olsonsjc@gmail.com>
2013-05-13 01:43:30 -0600

    User input changes

--- js/examples/user-input.js
+++ js/examples/user-input.js
@@ -35,38 +35,38 @@ $( window ).ready( function() {
   scene.updateScene();
 });
 
-$( window ).ready( function() {
-  var scene = new scenery.Scene( $( '#user-input2-scene' ), { renderer: 'svg' } );
-  scene.initializeStandaloneEvents();
+// $( window ).ready( function() {
+//   var scene = new scenery.Scene( $( '#user-input2-scene' ), { renderer: 'svg' } );
+//   scene.initializeStandaloneEvents();
   
-  var arrow = new scenery.Path( {
-    shape: kite.Shape.regularPolygon( 3, 20 ), fill: '#0f0', stroke: '#000'
-  } );
-  var section = new scenery.Node( { children: [
-    new scenery.Rectangle( 0, 0, 100, 100, {
-      fill: 'rgba(255,255,255,0.2)', stroke: '#000', lineWidth: 6
-    } ),
-    new scenery.Node( { x: 30, y: 50, children: [ arrow ] } ), // left
-    new scenery.Node( { x: 70, y: 50, children: [ arrow ] } )  // right
-  ] } );
-  scene.children = [
-    new scenery.Node( { children: [ section ], x: 40, y: 40 } ),
-    new scenery.Node( { children: [ section ], x: 140, y: 180,
-                        rotation: Math.PI / 2 } ),
-    new scenery.Node( { children: [ section ], x: 220, y: 90, 
-                        rotation: Math.PI / 4 } )
-  ];
+//   var arrow = new scenery.Path( {
+//     shape: kite.Shape.regularPolygon( 3, 20 ), fill: '#0f0', stroke: '#000'
+//   } );
+//   var section = new scenery.Node( { children: [
+//     new scenery.Rectangle( 0, 0, 100, 100, {
+//       fill: 'rgba(255,255,255,0.2)', stroke: '#000', lineWidth: 6
+//     } ),
+//     new scenery.Node( { x: 30, y: 50, children: [ arrow ] } ), // left
+//     new scenery.Node( { x: 70, y: 50, children: [ arrow ] } )  // right
+//   ] } );
+//   scene.children = [
+//     new scenery.Node( { children: [ section ], x: 40, y: 40 } ),
+//     new scenery.Node( { children: [ section ], x: 140, y: 180,
+//                         rotation: Math.PI / 2 } ),
+//     new scenery.Node( { children: [ section ], x: 220, y: 90, 
+//                         rotation: Math.PI / 4 } )
+//   ];
   
-  section.cursor = 'pointer';
-  section.addInputListener( new scenery.SimpleDragHandler() );
-  // _.each( scene.children, function( child ) { child.addInputListener( new scenery.SimpleDragHandler() ); } );
+//   section.cursor = 'pointer';
+//   section.addInputListener( new scenery.SimpleDragHandler() );
+//   // _.each( scene.children, function( child ) { child.addInputListener( new scenery.SimpleDragHandler() ); } );
   
-  // scene.addInputListener( { move: function( event ) { console.log( event.pointer.point.toString() ); } } );
+//   // scene.addInputListener( { move: function( event ) { console.log( event.pointer.point.toString() ); } } );
   
-  scene.centerX = 340/2;
-  scene.centerY = 300/2;
+//   scene.centerX = 340/2;
+//   scene.centerY = 300/2;
   
-  scene.timestep = function() {};
+//   scene.timestep = function() {};
   
-  scene.updateScene();
-});
+//   scene.updateScene();
+// });
